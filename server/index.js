93b6bdc77ff9ffed52b3ac003138dbfa7dff3254
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-03-16 01:04:36 +0530

    Added gitignore and created a simple express server

---
+++ server/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(PORT, () => console.log(`server running at PORT: ${PORT}`));
