0f528a88da032a56d354a0f4ba67ea74d37980f3
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-04-21 22:55:21 +0530

    created layout components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import IndexPage from "./pages/indexPage";
 import LoginPage from "./pages/LoginPage";
+import Layout from "./components/Layout";
 
 export default function App() {
   return (
     <Routes>
-      <Route path='/' element={<IndexPage />} />
-      <Route path='/login' element={<LoginPage />} />
+      <Route path='/' element={<Layout />}>
+        <Route path='/' element={<IndexPage />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Route>
     </Routes>
   );
 }
