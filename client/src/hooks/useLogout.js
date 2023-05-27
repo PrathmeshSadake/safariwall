6a549b2e70f253bd8ed0ece0c9b224b89ad726c6
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-05-27 17:42:31 +0530

    Created authentication context

---
+++ client/src/hooks/useLogout.js
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+
+export const useLogout = () => {
+  const { dispatch } = useContext(UserContext);
+
+  const logout = () => {
+    localStorage.removeItem("user");
+    dispatch({ type: "LOGOUT" });
+  };
+
+  return { logout };
+};
